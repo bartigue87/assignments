1c99cb1d84284e42c3ed819c731a53b60fd1524a
Brandon Artigue <bartigue87@yahoo.com>
2020-04-09 13:43:40 -0600

    code challenge

--- exercises/code-challenges/challenge1/app.js
+++ exercises/code-challenges/challenge1/app.js
@@ -2,12 +2,26 @@
 //     for(let i = 0; i < str.length - 1; i++){
 //         if(str[i] === str[i+1]){
 //             return true
-//         } 
+//         }
 
-//     } 
+//     }
 //     return false
 // }
 
- 
 //  console.log(hasDouble("racecar"))
 
+let magicButtonCount = 0;
+class MagicButton extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: "open" });
+    let which = magicButtonCount++;
+    const button = document.createElement("button");
+    button.onclick = (event) => {
+      alert("You clicked " + which + "!");
+    };
+    button.innerText = "Magic Button " + which;
+    shadow.appendChild(button);
+  }
+}
+customElements.define("magic-button", MagicButton);
